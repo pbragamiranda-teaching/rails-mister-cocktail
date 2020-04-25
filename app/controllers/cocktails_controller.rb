2659856d242e02b0a502fb89e28a7594985ce27f
PedroMiranda <pbragamiranda@gmail.com>
2020-04-25 16:52:09 -0300

    pushing cocktails

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,30 @@
+class CocktailsController < ApplicationController
+	before_action :find_cocktail, only: :show
+
+	def index
+		@cocktails = Cocktail.all
+	end
+
+	def show
+	end
+
+	def new
+		@cocktail = Cocktail.new
+	end
+
+	def create
+		@cocktail = Cocktail.new(cocktail_params)
+		@cocktail.save
+		redirect_to cocktails_path
+	end
+
+	private
+
+	def find_cocktail
+		@cocktail = Cocktail.find(params[:id])
+	end
+
+	def cocktail_params
+		params.require(:cocktail).permit(:name)
+	end
+end
